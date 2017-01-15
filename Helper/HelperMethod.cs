92f62e877c5384e85ca0f858414ddcc0a7221243
bsokat <barissokat@gmail.com>
2017-01-15 18:39:19 +0300

    Helper method added

---
+++ Helper/HelperMethod.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkDataModel.Helper
+{
+    public static class HelperMethod
+    {
+        public  static string ChangeCharacter(string text)
+        {
+            text = text.ToLower();
+            return text.Replace('ş', 's').
+                         Replace('ç', 'c').
+                         Replace('ğ', 'g').
+                         Replace('ü', 'u').
+                         Replace('ı', 'i').
+                         Replace('ö', 'o').
+                         Replace(' ', '-').
+                         Replace("?", "").
+                         Replace(",", "").
+                         Replace(".", "").
+                         Replace("\"", "").
+                         Replace("#", "").
+                         Replace("*", "-").
+                         Replace("'", "").
+                         Replace("/", "-").
+                         Replace("<", "").
+                         Replace(">", "").
+                         Replace(";", "").
+                         Replace(":", "").
+                         Replace("=", "").
+                         Replace("&", "").
+                         Replace("+", "").
+                         Replace("$", "").
+                         Replace("%", "").
+                         Replace("~", "").
+                         Replace("(", "").
+                         Replace(")", "").
+                         Replace("{", "").
+                         Replace("}", "").
+                         Replace("!", "").
+                         Replace("|", "").
+                         Replace("^", "").
+                         Replace("[", "").
+                         Replace("]", "").
+                         Replace("@", "");
+
+        }
+    }
+}
